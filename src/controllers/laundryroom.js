0b11e3687b87700e25d26d4122d640515be2dfac
Ali Canberk Anar <canberk.anar@tum.de>
2021-06-26 11:56:02 +0300

    LaundryRoom List ALL API

--- src/controllers/laundryroom.js
+++ src/controllers/laundryroom.js
@@ -1,29 +1,22 @@
 const LaundryRoom = require("../models/laundryroom");
 
-
-const read = async (req, res) => {
+const list = async (req, res) => {
     try {
-        // get laundryRoom with id from database
-        let laundryRoom = await LaundryRoom.findById(req.params.id).exec();
+        // get all laundryrooms in database
+        let laundryRooms = await LaundryRoom.find({}).exec();
 
-        // if no laundryRoom with id is found, return 404
-        if (!movie)
-            return res.status(404).json({
-                error: "Not Found",
-                message: `Movie not found`,
-            });
-
-        // return gotten laundry
-        return res.status(200).json(movie);
+        // return gotten movies
+        return res.status(200).json(laundryRooms);
     } catch (err) {
         console.log(err);
         return res.status(500).json({
-            error: "Internal Server Error",
+            error: "Internal server error",
             message: err.message,
         });
     }
 };
 
+
 module.exports = {
-    read
+    list
 };
--- src/routes/laundryroom.js
+++ src/routes/laundryroom.js
@@ -6,6 +6,6 @@ const router = express.Router();
 const middlewares = require("../middlewares");
 const LaundryController = require("../controllers/laundryroom")
 
-router.get("/laundryroom/:id", LaundryController.read);
+router.get("/", LaundryController.list);
 
 module.exports = router;
