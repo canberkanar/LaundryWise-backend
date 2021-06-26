a7dc3a06d726249b551a4ebf8a6fc7d14d50481e
Ayberk Tecimer <ayberktecimer@yahoo.com>
2021-06-26 09:14:19 +0200

    models ctd.

--- src/models/rental.js
+++ src/models/rental.js
@@ -3,17 +3,42 @@
 const mongoose = require("mongoose");
 const laundry_room = require("laundryroom");
 
+const AllocatedTimeSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true,
+        index: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    startTime: {
+        type: Date,
+        required: true,
+    },
+    endTime: {
+        type: Date,
+        required: true,
+    },
+});
+
+
 const Rental = new mongoose.Schema({
     id: {
         type: String,
         required: true,
         index: true,
     },
+    machine: MachineSchema,
+    allocated_time: AllocatedTimeSchema,
     title: {
         type: String,
         required: true
     },
-    body: {
-        type: String
-    },
+    payment: PaymentSchema,
+    feedback: FeedbackSchema,
 });
+
+
+
