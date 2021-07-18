daec27dc24fddb3a82a49fa545e40417c1d1f55d
abcdrk <kdreyyan@gmail.com>
2021-07-18 22:24:08 +0200

    Statistics controller can get the related rentals of the service provider and list it

--- src/controllers/statistics.js
+++ src/controllers/statistics.js
@@ -3,20 +3,17 @@ const {Rental} = require("../models/rental");
 const list = async (req, res) => {
     try {
 
-        const machine_list = req.machines;
-        // get all rentals
-        let rentals = await Rental.find({}).exec();
+        let rentals = await Rental.find({ serviceProvider: req.body.serviceProvider }).exec();
 
         // rentals.filter(r => r.machine === machine_list).map(r => r.payment)
 
-
-        let total_revenue = rentals.filter(r => r.machine === machine_list).map(r => r.payment);
+        // let total_revenue = rentals.filter(r => r.machine === machine_list).map(r => r.payment);
         // rentals.forEach(obj => {
         //     total_revenue += obj.payment.cost;
         // })
 
         // return total revenue
-        return res.status(200).json(total_revenue);
+        return res.status(200).json(rentals);
     } catch (err) {
         console.log(err);
         return res.status(500).json({
