78725c6f5c653b1cc3142c06674bb4a6c088ad1b
abcdrk <kdreyyan@gmail.com>
2021-07-21 23:18:04 +0200

    created listInRoom API and updated create announcement

--- src/controllers/announcement.js
+++ src/controllers/announcement.js
@@ -16,25 +16,28 @@ const list = async (req, res) => {
     }
 };
 
-// Give the laundryRoomId as req.
+// Give the laundryRoomId as req.body
 const listInRoom = async (req, res) => {
     try {
-        // Getting all announcements of the room.
+        console.log(req.query.id)
         let room = await LaundryRoom.findById(req.body.laundryRoomId).exec();
-        let announcements = room.announcements;
+        let announcements_list = room.announcements;
         console.log("Getting all announcements of the room.");
-        console.log(announcements);
-        return res.status(200).json(announcements);
+        let announcements = []
+        for(let a_id of announcements_list){
+            let m = await Announcement.findById(a_id).exec();
+            announcements.push(m);
+        }
 
+        return res.status(200).json(announcements);
     } catch (err) {
         console.log(err);
         return res.status(500).json({
-            error: "Internal server error on listing Announcements",
+            error: "Internal server error",
             message: err.message,
         });
     }
 };
-
 
 const read = async (req, res) => {
     try {
@@ -66,113 +69,41 @@ const create = async (req, res) => {
 
     // handle the request
     try {
-        // create movie in database
-        let announcement = await Announcement.create(req.body);
-
-        // return created movie
-        return res.status(201).json(announcement);
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({
-            error: "Internal server error",
-            message: err.message,
-        });
-    }
-};
-
-const createx = async (req, res) => {
-    // check if the body of the request contains all necessary properties
-    if (Object.keys(req.body).length === 0)
-        return res.status(400).json({
-            error: "Bad Request",
-            message: "The request body is empty",
-        });
-
-    // handle the request
-    try {
         try {
-
-            if (req.body.deviceRoomId == null) {
-                console.log("RoomID Machine belongs to must be given!");
+            if (req.body.laundryRoomId == null) {
+                console.log("RoomID Announcement belongs to must be given!");
                 return res.status(500).json({
-                    error: "RoomID Machine belongs to must be given!",
-                    message: "RoomID Machine belongs to must be given!",
+                    error: "RoomID Announcement belongs to must be given!",
+                    message: "RoomID Announcement belongs to must be given!",
                 });
             }
 
-            let laundryRoom = await LaundryRoom.findById(req.body.deviceRoomId).exec();
+            let laundryRoom = await LaundryRoom.findById(req.body.laundryRoomId).exec();
 
             if (laundryRoom == null) {
-                console.log("RoomID Machine belongs to does not exist!");
+                console.log("RoomID Announcement belongs to does not exist!");
                 return res.status(500).json({
-                    error: "RoomID Machine belongs to does not exist!",
-                    message: "RoomID Machine belongs to does not exist!",
+                    error: "RoomID Announcement belongs to does not exist!",
+                    message: "RoomID Announcement belongs to does not exist!",
                 });
             }
+        } catch {}
 
-            const roomOperationStartHour = laundryRoom.toJSON().operationStartHour;
-            const roomOperationEndHour = laundryRoom.toJSON().operationEndHour;
-
-            let today = new Date();
-            let newStart = new Date();
-            let newEnd = new Date();
-
-            today.setHours(0, 0, 0, 0);
-            newStart.setHours(0, 0, 0, 0);
-            newEnd.setHours(0, 0, 0, 0);
-
-            for (i = 0; i < numberOfDaysToGenerate; i++) {
-                for (j = 0; j < 24; j++) {
-
-                    newStart.setHours(j)
-                    newEnd.setHours(1 + j)
-
-                    let newStatus = "outOfService";
-                    if (j >= roomOperationStartHour && j < roomOperationEndHour) {
-                        newStatus = "available";
-                    }
-
-                    let slot = {
-                        date: new Date(today),
-                        startTime: new Date(newStart),
-                        endTime: new Date(newEnd),
-                        status: newStatus
-                    };
-
-                    let pushedTimeSlot;
-                    pushedTimeSlot = await TimeSlot.create(slot);
-                    timeslots.push(pushedTimeSlot); // new object creation is needed since values are overwritten due to pass by reference
-                }
-
-                today.setDate(today.getDate() + 1);
-                newStart.setDate(today.getDate() );
-                newEnd.setDate(today.getDate())
-            }
-
-            req.body.timeslots = timeslots; // merge created timeslots with create machine request
-
-        } catch (err) {
-            console.log(err);
-            return res.status(500).json({
-                error: "Internal Server Error",
-                message: err.message,
-            });
+        let data = {
+            "title": req.body.title,
+            "body": req.body.body
         }
+        let ann = await Announcement.create(data);
+        let added_laundryroom = await LaundryRoom.findOneAndUpdate(
+            {_id: req.body.laundryRoomId},
+            {$push: {announcements: ann}});
+
+        console.log("ADDED ANNOUNCEMENT TO LAUNDRY ROOM");
+        console.log(added_laundryroom);
 
 
-        // create movie in database
-        let m = await Machine.create(req.body);
-        //const xx = req.body.deviceRoomId;
-        let added_machine = await LaundryRoom.findOneAndUpdate(
-            {_id: req.body.deviceRoomId},
-            {$push: {machines: m}});
-
-        console.log(added_machine);
-
-        //LaundryRoom.updateOne({"id": req.body.deviceRoomId},)
-        // handle the request
         // create machine in database
-        return res.status(201).json(m);
+        return res.status(201).json(ann);
 
     } catch
         (err) {
