5c2b05b481b694d655f9be2fe2bd0286104c9b90
ahmettalhasen <talhasen@protonmail.com>
2021-07-23 00:21:37 +0200

    first commit

--- src/controllers/statistics.js
+++ src/controllers/statistics.js
@@ -1,9 +1,13 @@
 const {Rental, Payment} = require("../models/rental");
 
-const list = async (req, res) => {
+const getStatistics = async (req, res) => {
     try {
 
-        let rentals = await Rental.find({ serviceProvider: req.body.serviceProvider }).exec();
+        let rentals = await Rental.find(
+            {
+                serviceProvider: req.body.serviceProviderId
+            }
+        ).exec();
         console.log(rentals);
         // let x = await Payment.findById("60f4866660b4fd1c7d1af80d").exec();
         // console.log(x.cost)
@@ -15,19 +19,6 @@ const list = async (req, res) => {
             let p = await Payment.findById(r.payment).exec();
             costs.push(p.cost);
         }
-
-        // console.log(payments);
-        // let total_revenue = 0;
-        // for (let obj of payments){
-        //     console.log(obj.cost);
-        // }
-        /*
-        payments.forEach(obj => {
-            // total_revenue += obj.cost;
-            console.log(obj.cost);
-        })*/
-
-        // return total revenue
         return res.status(200).json(costs);
     } catch (err) {
         console.log(err);
@@ -39,6 +30,7 @@ const list = async (req, res) => {
 };
 
 
+
 module.exports = {
-    list
+    getStatistics
 };
